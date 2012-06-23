ee53f7a17c3a159f32b01991a216c3cc3b8c17fb
mpower <mpower@dodtsair.com>
2012-06-22 22:52:05 -0700

    Initial auto generated projects

---
+++ birdseye/birdseye-jsonp/src/main/java/org/boazglean/kathab/birdseye/App.java
@@ -0,0 +1,13 @@
+package org.boazglean.kathab.birdseye;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
