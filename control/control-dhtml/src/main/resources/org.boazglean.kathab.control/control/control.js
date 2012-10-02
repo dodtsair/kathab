83eef5a5a2095a0eda2460275b6449f3c548742c
mpower <mpower@dodtsair.com>
2012-10-02 14:07:45 -0700

    control now works with require.js

--- control/control-dhtml/src/main/resources/org.boazglean.kathab.control/control/control.js
+++ control/control-dhtml/src/main/resources/org.boazglean.kathab.control/control/control.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * The MIT License
  *
  * Copyright 2012 mpower.
@@ -22,64 +22,87 @@
  * THE SOFTWARE.
  */
 
-$(document).ready(function() {
+
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: '..',
+    shim: {
+        'com.beebole.pure/pure-js/pure': {
+            //These script dependencies should be loaded before loading
+            //pure.js
+            deps: ['org.jquery/jquery-js/jquery'],
+        },
+        'org.jquery/jquery-sparkline/jquery.sparkline': {
+            //These script dependencies should be loaded before loading
+            //pure.js
+            deps: ['org.jquery/jquery-js/jquery'],
+        },
+    }
+});
+
+requirejs(['org.jquery/jquery-js/jquery', 'com.beebole.pure/pure-js/pure', 'org.jquery/jquery-sparkline/jquery.sparkline'],
+function   () {
     console.log("begin control.js");
-    var prefixRender = $('.prefix-filter').compile(
-        {
-            '.prefix-entry': {
-                'point<-points' :{
-                    'div.prefix-name':'point.key',
-                    'div.prefix-bar@data-values': function(arg) {
-                        var sparkline = [];
-                        sparkline.push(arg.context.mean);
-                        sparkline.push(arg.context.points[arg.pos].value);
-                        sparkline.push(arg.context.mean + arg.context.stdDeviation*2);
-                        sparkline.push(arg.context.mean + arg.context.stdDeviation);
-                        sparkline.push(arg.context.mean - arg.context.stdDeviation);
-                        return sparkline.join();
+    $(document).ready(function() {
+        var prefixRender = $('.prefix-filter').compile(
+            {
+                '.prefix-entry': {
+                    'point<-points' :{
+                        'div.prefix-name':'point.key',
+                        'div.prefix-bar@data-values': function(arg) {
+                            var sparkline = [];
+                            sparkline.push(arg.context.mean);
+                            sparkline.push(arg.context.points[arg.pos].value);
+                            sparkline.push(arg.context.mean + arg.context.stdDeviation*2);
+                            sparkline.push(arg.context.mean + arg.context.stdDeviation);
+                            sparkline.push(arg.context.mean - arg.context.stdDeviation);
+                            return sparkline.join();
+                        }
+                    },
+                    sort:function(a,b) {
+                        return b.value - a.value;
                     }
                 },
-                sort:function(a,b) {
-                    return b.value - a.value;
-                }
-            },
-        }
-    )
+            }
+        )
 
-    var levelRender = $('.level-filter').compile(
-    {
-        '.@data-values' : function(arg) {
-            var pieKeys = [];
-            var pieData = [];
-            for(var dataPoint in arg.context) {
-                pieKeys.push(dataPoint);
+        var levelRender = $('.level-filter').compile(
+        {
+            '.@data-values' : function(arg) {
+                var pieKeys = [];
+                var pieData = [];
+                for(var dataPoint in arg.context) {
+                    pieKeys.push(dataPoint);
+                }
+                pieKeys.sort();
+                for(var keyPos in pieKeys) {
+                    pieData.push(arg.context[pieKeys[keyPos]])
+                }
+                return pieData.join();
             }
-            pieKeys.sort();
-            for(var keyPos in pieKeys) {
-                pieData.push(arg.context[pieKeys[keyPos]])
-            }
-            return pieData.join();
-        }
-    });
-    $(document).on("summary/level", function(event, data) {
-        $('.level-filter').replaceWith(levelRender(data));
-        $(".level-filter").sparkline('html', {
-            type: 'pie',
-            sliceColors: ['rgba(0,0,256,100)','rgba(256,0,0,100)','rgba(0,256,0,100)','rgba(256,256,256,100)','rgba(256,256,0,100)'],
-            width: '100%',
-            height: '100%',
-            tagValuesAttribute: 'data-values',
         });
-    });
-    $(document).on("summary/prefix", function(event, data) {
-        $('.prefix-filter').replaceWith(prefixRender(data));
-        $('.prefix-bar').sparkline('html', {
-            type: 'bullet',
-            targetColor: 'rgba(0,0,0,100)',
-            rangeColors: ['rgba(100,100,100,100)','rgba(66,66,66,100)','rgba(33,33,33,100)'],
-            width: '100%',
-            tagValuesAttribute: 'data-values',
+        $(document).on("summary/level", function(event, data) {
+            $('.level-filter').replaceWith(levelRender(data));
+            $(".level-filter").sparkline('html', {
+                type: 'pie',
+                sliceColors: ['rgba(0,0,256,100)','rgba(256,0,0,100)','rgba(0,256,0,100)','rgba(256,256,256,100)','rgba(256,256,0,100)'],
+                width: '100%',
+                height: '100%',
+                tagValuesAttribute: 'data-values',
+            });
         });
+        $(document).on("summary/prefix", function(event, data) {
+            $('.prefix-filter').replaceWith(prefixRender(data));
+            $('.prefix-bar').sparkline('html', {
+                type: 'bullet',
+                targetColor: 'rgba(0,0,0,100)',
+                rangeColors: ['rgba(100,100,100,100)','rgba(66,66,66,100)','rgba(33,33,33,100)'],
+                width: '100%',
+                tagValuesAttribute: 'data-values',
+            });
+        });
+        console.log("end control.js");
+        $.getScript('../../api/summary/level')
+        $.getScript('../../api/summary/prefix')
     });
-    console.log("begin control.js");
 });
